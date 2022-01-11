05ebdcbac16c24bb9e9daabe4d531676bb228f31
Eliac11 <58089493+Eliac11@users.noreply.github.com>
2022-01-11 22:20:21 +0300

    Initial commit

---
+++ Assets/Scenes/Video.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Video : MonoBehaviour
+{
+
+    WebCamTexture webCamTexture;
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        webCamTexture.Play();
+    }
+
+    void Update()
+    {
+        GetComponent<RawImage>().texture = webCamTexture;
+    }
+}
